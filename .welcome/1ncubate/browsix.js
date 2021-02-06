9cf7f39fc55557da058d3062c2927ffded63b1d9
harrison.cross <harrison.cross@transport4.com>
2021-02-05 19:30:46 -0500

    incubate browsix (for posix system commands, etc)

---
+++ .welcome/1ncubate/browsix.js
@@ -0,0 +1,3 @@
+// https://unpkg.com/browse/browsix@0.9.2/lib-dist/lib/kernel/kernel.js
+// https://unpkg.com/browse/browsix@0.9.2/lib-dist/lib/syscall-api/syscall-api.js
+// https://unpkg.com/browse/browsix@0.9.2/lib-dist/lib/browser-node/browser-node.js
